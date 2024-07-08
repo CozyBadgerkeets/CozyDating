e41cc8bcbd265609bf343cca214abe18b5b759cd
MarioV <mvaquero143@gmail.com>
2024-07-08 17:10:44 -0400

    Planned Dialogue System

---
+++ Assets/DialogueScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueScript : MonoBehaviour
+{
+    // Different systems for different characters
+    // all with have their own array in different scenes
+    // so that there isnt too much clutter
+
+    // create a loop that takes lines from a text document and puts them into a specfic array depending on who said it
+
+    public string[] BastetSpeechSystem;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
